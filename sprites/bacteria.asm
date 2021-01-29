9e87b2c31f81fbc6ec4fa0c019851a6b3077c736
Billy Rebecchi <billy.rebecchi@googlemail.com>
2021-01-29 20:11:30 +0000

    Add mushroom, bacteria and virus sprites

---
+++ sprites/bacteria.asm
@@ -0,0 +1,5 @@
+SECTION "BACTERIA", ROM0
+
+Bacteria:
+	DB $12,$12,$14,$14,$79,$79,$4e,$76
+	DB $62,$5e,$aa,$b6,$3e,$3e,$48,$48
---
+++ sprites/mushyBig.asm
@@ -0,0 +1,5 @@
+SECTION "MUSHYBIG", ROM0
+
+MushyBig:
+	DB $7c,$7c,$82,$fe,$01,$c7,$00,$83
+	DB $10,$18,$10,$18,$30,$3c,$70,$7e

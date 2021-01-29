24903f5e8b2f1f5a0ba58bc5c02071fe7a168009
Billy Rebecchi <billy.rebecchi@googlemail.com>
2021-01-29 23:02:22 +0000

    Setup level loader with placeholder tiledata

--- sprites/bacteria.asm
+++ sprites/bacteria.asm
@@ -1,5 +1,5 @@
-SECTION "BACTERIA", ROM0
+SECTION "Bacteria", ROM0
 
-Bacteria:
+BACTERIA:
 	DB $12,$12,$14,$14,$79,$79,$4e,$76
 	DB $62,$5e,$aa,$b6,$3e,$3e,$48,$48
--- sprites/mushySmall.asm
+++ sprites/mushySmall.asm
@@ -1,5 +1,5 @@
-SECTION "MUSHYSMALL", ROM0
+SECTION "MushySmall", ROM0
 
-MushySmall:
+MUSHYSMALL:
 	DB $1c,$1c,$22,$22,$5d,$41,$bf,$81
 	DB $d3,$99,$10,$18,$30,$3c,$60,$7e

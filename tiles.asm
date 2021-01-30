0030a47672ed810a1b7cbb0ed354df8e590687d9
Billy Rebecchi <billy.rebecchi@googlemail.com>
2021-01-30 22:21:09 +0000

    Player sprite (#9)
    * Create player sprite
    
    * Add floor

--- tiles.asm
+++ tiles.asm
@@ -9,5 +9,9 @@ TILES:
     ; ------- PLATFORM --------
     DB $FF,$FF,$E7,$7A,$FF,$10,$FB,$46
     DB $F7,$2A,$FF,$20,$F3,$0C,$70,$FF
+    
+    ; ------- FLOOR --------
+	DB $ff,$00,$ff,$00,$ff,$00,$00,$ff
+	DB $00,$ff,$00,$ff,$ff,$ff,$ff,$ff
 
 TILESEND:

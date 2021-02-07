e8024bcf476a64f6cfc30e2fb1ccaf0c9363a076
Billy Rebecchi <billy.rebecchi@googlemail.com>
2021-02-07 14:39:55 +0000

    Add ability to display map data

--- level-editor/main.js
+++ level-editor/main.js
@@ -8,10 +8,18 @@ var selectedTab;
 var noTilesX = 16;
 var noTilesY = 8;
 
+// Store the number of map tiles
+var noMapTilesX = 200;
+var noMapTilesY = 13;
+
 // Store the hex data for the tiles and map
 var editorData = { "tiles" : "<-- PASTE TILE DATA HERE -->", "map" : "<-- PASTE MAP DATA HERE -->" };
 var tileData = Array(noTilesX * noTilesY);
-var mapData = [];
+var mapData = Array(noMapTilesX * noMapTilesY);
+
+// Store offset vertical and horizontal (for scroll wheel)
+var offsetTiles = 0;
+var offsetMap = 0;
 
 // Set pixel colours
 var pixelColours = { "00":255, "01":170, "10":85, "11":0 };
@@ -45,7 +53,7 @@ function drawTiles() {
 }
 
 function drawLevelMap() {
-    
+    drawMapGrid();
 }
 
 function selectTab(tab) {
@@ -64,6 +72,7 @@ function initEditorTextChangeListener() {
         editorData[selectedTab] = newData;
         document.getElementById("editorText").value = newData;
         if (selectedTab == "tiles") decodeTileData(newData);
+        else decodeMapData(newData);
     });
 }
 
@@ -91,6 +100,36 @@ function drawTile(x, y, width, height) {
     }
 }
 
+function drawMapGrid() {
+    for (var i = 0; i < noMapTilesX; i++) {
+        for (var j = 0; j < noMapTilesY; j++) {
+            drawMapTile(i, j, holder.offsetHeight / noMapTilesY, holder.offsetHeight / noMapTilesY, noMapTilesY);
+        }
+    }
+}
+
+function drawMapTile(x, y, width, height) {
+    stroke(0,0,0);
+    drawRect(offsetMap + x * width, y * height, width, height, 255);
+    var column = mapData[x];
+    if (column) {
+        var tilePos = column[y];
+        if (tilePos) {
+            var rowData = tileData[tilePos];
+            if (rowData) {
+                var pixelWidth = width / 8;
+                var pixelHeight = height / 8;
+                for (var i = 0; i < 8; i++) {
+                    for (var j = 0; j < 8; j++) {
+                        noStroke();
+                        drawRect(offsetMap + x * width + j * pixelWidth, y * height + i * pixelHeight, pixelWidth, pixelHeight, pixelColours[rowData[i * 8 + j]]);
+                    }
+                }
+            }
+        }
+    }
+}
+
 function drawRect(x, y, width, height, colour) {
     fill(colour);
     quad(x,y, x+ width,y, x + width,y+height, x,y+height);
@@ -112,6 +151,27 @@ function decodeTileData(dataString) {
     }
 }
 
+function decodeMapData(dataString) {
+    var separatedTiles = dataString.replaceAll("DB ", "").replaceAll("$", "").split("\n");
+    for (var i = 0; i < separatedTiles.length; i++) {
+        var rowData = [];
+        var tilePixelData = separatedTiles[i].split(",");
+        for (var j = 0; j < tilePixelData.length; j++) {
+            rowData.push(parseInt(tilePixelData[j], 16));
+        }
+        mapData[i] = rowData;
+    }
+}
+
 function hexToBinary(hex){
     return ("00000000" + (parseInt(hex, 16)).toString(2)).substr(-8);
+}
+
+function mouseWheel(event) {
+    if (selectedTab == "tiles") {
+        
+    } else {
+        offsetMap += event.delta * 0.1;
+        if (offsetMap > 0) offsetMap = 0;
+    }
 }
